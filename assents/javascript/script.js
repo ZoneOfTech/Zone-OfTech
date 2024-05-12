dc73c259170446b34ddc92e5ba56c4cd7387f1f7
ZoneOfTech <133525922+ZoneOfTech@users.noreply.github.com>
2024-05-11 23:03:05 -0300

    commit init
    Site para carros da ZOne Of Tech

---
+++ assents/javascript/script.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+autoSlide();
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function nextSlide() {
+  showSlides(slideIndex += 1);
+}
+
+function autoSlide() {
+  setInterval(nextSlide, 3000); // Muda o slide a cada 5 segundos
+}
